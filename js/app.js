992a405513a2b04edf349aa9519d5d270b50a3ad
Mark VanLandingham <mark@laborchart.com>
2017-09-06 16:50:12 -0500

    Reworked structure

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var createEvent = function() {   
+   data={
+      name: document.getElementById('name').value
+   }
+   $.ajax({
+      url: "http://localhost:8080/event",
+      data: JSON.stringify(data),
+      method: "POST",
+      contentType: 'application/json',
+      dataType: "json",
+      success: function(data){
+         console.log("success")
+         dataType: "json"
+      },
+   })
+   document.getElementById('name').value = ""
+}
+
+document.getElementById("btn").addEventListener("click", createEvent);
---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser')
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use('/css',express.static(__dirname + '/css'));
+app.use('/js',express.static(__dirname + '/js'));
+app.use('/assets',express.static(__dirname + '/assets'));
+
+app.listen(8080);
+
+var Airtable = require('airtable');
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: 'keyz6nhx5XT4NyMUp'
+});
+var base = Airtable.base('app2SkZOQcF0m2jZG');
+
+app.post("/event", function(request,response){
+   base('Events').create({
+      "Name": request.body.name
+   }, function(err, record){
+      if(err) {
+         console.error(err); return;
+      }
+      console.log(record.getId())
+   })
+})
