b93b0b1e96c393d24c7819af8d1bd33506ca1bb2
Alex Bohlken <alexbohlken@gmail.com>
2017-09-07 13:38:46 -0500

    Added two placeholder methods, getEvents and drawEvents

--- js/date-vm.js
+++ js/date-vm.js
@@ -2,3 +2,24 @@ var pageDate = window.location.href
 var res = pageDate.split('/')
 pageDate = res[4]
 console.log(pageDate);
+
+
+//pre: events airtable is sorted, 
+function getEvents(formattedDate)
+{
+	//create an array to hold references to airtable rows
+	let eventsArr;
+	
+	//use airtable to find events on day
+	
+	//push event references to array
+	
+	return eventsArr;
+}
+
+function drawEvents()
+{
+	let formDate;
+	let eventsArr = getEvents(formDate);
+	//fancy DOM editting magic
+}
